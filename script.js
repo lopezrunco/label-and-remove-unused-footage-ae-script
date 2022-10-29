e862951f3dc1ebc8a32234b973b4c424a8a2bc3e
Damián López <lopezrunco@gmail.com>
2022-10-29 20:07:40 +0530

    code optimization

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var footageItems = getFootage()
 var mainWindow = new Window('palette', 'Label and Remove Unused Items', undefined)
 mainWindow.orientation = 'row'
 var labelBtn = mainWindow.add('button', undefined, 'Label used & unused')
-var removeAllBtn = mainWindow.add('button', undefined, 'Remove all unused')
+var removeAllBtn = mainWindow.add('button', undefined, 'Remove unused files')
 
 labelBtn.onClick = function() {
     labelFootage(comps, footageItems)
@@ -21,17 +21,7 @@ mainWindow.show()
 // Logic
 function labelFootage(comps, footages) {
     app.beginUndoGroup('Identify Used Footage')
-    for (var i = 0; i < comps.length; i++) {
-        for (var l = 1; l <= comps[i].numLayers; l++) { // iterate the comp layers
-            if (comps[i].layer(l).source) { // if layer has a source
-                for (var f = 0; f < footages.length; f++) { // iterate footage items & compare each one to see if it exists, if true, add an 'On use' comment
-                    if (comps[i].layer(l).source == footages[f]) {
-                        comps[i].layer(l).source.comment = 'On use'
-                    }
-                }
-            }
-        }
-    }
+    commentUsed(comps, footages)
     // add 'Unused' comment
     for (var i = 0; i < footages.length; i++) {
         if(footages[i].comment != 'On use') {
@@ -43,17 +33,7 @@ function labelFootage(comps, footages) {
 
 function removeUnusedFootage(comps, footages) {
     app.beginUndoGroup('Remove Unused Footage')
-    for (var i = 0; i < comps.length; i++) {
-        for (var l = 1; l <= comps[i].numLayers; l++) { // iterate the comp layers
-            if (comps[i].layer(l).source) { // if layers has a source
-                for (var f = 0; f < footages.length; f++) { // iterate footage items & compare each one to see if it exists, if true, add an 'On use' comment
-                    if (comps[i].layer(l).source == footages[f]) {
-                        comps[i].layer(l).source.comment = 'On use'
-                    }
-                }
-            }
-        }
-    }
+    commentUsed(comps, footages)
     // delete unused footage
     for (var i = 0; i < footages.length; i++) {
         if(footages[i].comment != 'On use') {
@@ -83,4 +63,18 @@ function getFootage() {
         }
     }
     return array
+}
+
+function commentUsed(comps, footages) {
+    for (var i = 0; i < comps.length; i++) {
+        for (var l = 1; l <= comps[i].numLayers; l++) { // iterate the comp layers
+            if (comps[i].layer(l).source) { // if layers has a source
+                for (var f = 0; f < footages.length; f++) { // iterate footage items & compare each one to see if it exists, if true, add an 'On use' comment
+                    if (comps[i].layer(l).source == footages[f]) {
+                        comps[i].layer(l).source.comment = 'On use'
+                    }
+                }
+            }
+        }
+    }
 }
